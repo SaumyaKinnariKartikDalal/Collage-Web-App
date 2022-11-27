fdd4033a4e9e09ade0ae56f6dcfe02261cbd114e
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2022-11-27 18:19:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var Speech = window.webkitSpeechRecognition;
 var Recognition = new Speech();
+var camera = document.getElementById("camera");
 
-function Start (){
+function Start() {
     document.getElementById("textbox").innerHTML = "";
     Recognition.start();
 }
@@ -12,5 +13,56 @@ Recognition.onresult = function (event) {
     console.log(content);
 
     document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie") {
+        console.log("taking selfie");
+        speak();
+    }
+    
+}
 
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "Taking Your First Selfie in Five Seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snap("img1");
+        speak_data = "Taking Your Second Selfie in next Five Seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
+    
+    
+    setTimeout(function(){
+        take_snap("img2");
+        speak_data = "Taking Your Third Selfie in next Five Seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 10000);
+    
+    
+    setTimeout(function(){
+        take_snap("img3");
+    }, 15000);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality: 90
+});
+
+function take_snap(imgid){
+    Webcam.snap(function(data_uri){
+        document.getElementById(imgid).src = data_uri;
+    })
+}
+
+function save () {
+    link = document.getElementById("link");
+    link.href = document.getElementById("selfie").src;
+    link.click();
+    
 }
